f5755caee503ff81a884921e0f01785b1f4ad54b
Tomasz Zembok <tomasz.zembok@gmail.com>
2022-11-28 13:48:55 +0100

    new kata 2022.11.28

---
+++ 6 kyu/test_even_fibonacci_sum.py
@@ -0,0 +1,11 @@
+from even_fibonacci_sum import even_fib
+
+def test_1():
+    assert even_fib(1) == 0
+
+def test_2():
+    assert even_fib(10) == 10
+
+def test_3():
+    assert even_fib(5) == 2
+
